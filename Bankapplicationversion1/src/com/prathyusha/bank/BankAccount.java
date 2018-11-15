d6919c138c6ea8d09cd9cc98e47f7e49942fcf4f
164411Prathyusha <prathyusha14bf1a1205@gmail.com>
2018-11-15 18:17:24 +0530

    BankApplication Push

---
+++ Bankapplicationversion1/src/com/prathyusha/bank/BankAccount.java
@@ -0,0 +1,111 @@
+package com.prathyusha.bank;
+
+import java.util.Set;
+
+
+
+public class BankAccount {
+   //static variable
+	private static int autoAccountNo;
+	 // instance variable
+		private static int accountNo;
+		private String accoutHolderName;
+		private double accountBalance;
+		//init block
+		{
+     //System.out.println("init block");
+			accountNo=++autoAccountNo;
+		}
+		
+		// default constructor
+   public BankAccount(){
+	
+	 accoutHolderName = "unknown";
+	 accountBalance=0;
+	// System.out.println("default constructor");
+   }
+  
+	 //parameterized constructor overloading
+	 public BankAccount(String accoutHolderName,double accountBalance){
+		
+		//setter method --modify/re-intialise member variable
+		 
+		 this.accoutHolderName =accoutHolderName;
+		 this.accountBalance=accountBalance;
+		// System.out.println("parameterized constructor");
+	 }
+ //setter method
+	public void setAccoutHolderName(String accoutHolderName) {
+		this.accoutHolderName = accoutHolderName;
+	}
+	//getter methods
+	 public static int getAccountNo() {
+		return accountNo;
+	}
+	 public String getAccoutHolderName() {
+		return accoutHolderName;
+	}
+	 public double getAccountBalance() {
+		return accountBalance;
+	}
+	 //service methods
+	 public void withdraw(double amount){
+		 if(checkNegativeNum(amount)){
+			 System.out.println("Enter valid number");
+			 return;
+		 }
+		 if(checkGreaterNum(amount)){
+			 System.out.println("Enter current amount available in account");
+			 return;
+		 }
+		 if(checkMinBlnc(amount)){
+			 System.out.println("Check Minimum balance");
+			 return;
+		 }
+		 if(checkLessNum(amount)){
+			 System.out.println("Please, Enter minimum amt to withdraw");
+			 return;
+		 }
+		 this.accountBalance-=amount;
+		 
+	 }
+	 public void deposit(double amount){
+		 if(checkNegativeNum(amount)){
+			 System.out.println("Enter valid number");
+			 return;
+		 }
+		 this.accountBalance+=amount;
+	 }
+	 public boolean checkNegativeNum(double amount){
+		 if(amount<0){
+			return true;
+		 }
+		return false;
+	 }
+	 public boolean checkGreaterNum(double amount){
+		 if(amount>this.accountBalance){
+			 return true;
+		 }
+		 return false;
+	 }
+	 public boolean checkMinBlnc(double amount){
+		 if(amount==1000){
+			 return true;
+		 }
+		 return false;
+	 }
+	 public boolean checkLessNum(double amount){
+		 if(amount<100){
+			 return true;
+		 }
+		 return false;
+	 }
+	 public static void main(String[] args) {
+		BankAccount acc=new BankAccount();
+		BankAccount acc1=new BankAccount("prathyusha",1500);
+		acc1.withdraw(2000);
+		acc1.deposit(-1);
+	}
+	
+
+}
---
+++ Bankapplicationversion1/src/com/prathyusha/test/TestBankAccount.java
@@ -0,0 +1,13 @@
+package com.prathyusha.test;
+import com.prathyusha.bank.*;
+public class TestBankAccount {
+            
+	 public static void testBankAccountMethod(){
+		 BankAccount acc=new BankAccount();
+		 acc.setAccoutHolderName("Bhuvaneswar");
+	 }
+	
+public static void main(String args[]){
+     testBankAccountMethod();
+}
+}
